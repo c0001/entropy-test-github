a9f16b9df2189aadd30eae1a915d62d6602a3e96
Entroperiance.J <bmsac0001@gmail.com>
2023-08-31 21:05:52 +0800

    Going on ...

--- .free-github-action-disk-space@ubuntu.sh
+++ .free-github-action-disk-space@ubuntu.sh
@@ -28,10 +28,9 @@
 echo "=============================================================================="
 echo "Freeing up disk space on CI system"
 echo "=============================================================================="
-
+df -h
 echo "Listing 100 largest packages"
 dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -n | tail -n 100
-df -h
 echo "Removing large packages"
 sudo apt-get remove -y '^ghc-8.*'
 sudo apt-get remove -y '^dotnet-.*'
